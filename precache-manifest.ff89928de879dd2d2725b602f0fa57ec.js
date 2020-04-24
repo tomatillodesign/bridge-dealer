3cbc975845b5f5b1b137214099336f8569bfae7b
Chris Liu-Beers <chris@tomatillodesign.com>
2020-04-24 16:39:35 -0400

    Updates

---
+++ precache-manifest.ff89928de879dd2d2725b602f0fa57ec.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5a17659c30277e131e3aa13e091d1cf",
+    "url": "/bridge-dealer/index.html"
+  },
+  {
+    "revision": "f643523fe9804929dc55",
+    "url": "/bridge-dealer/static/css/main.d93963ab.chunk.css"
+  },
+  {
+    "revision": "482b928f2474d41f1709",
+    "url": "/bridge-dealer/static/js/2.88bb370f.chunk.js"
+  },
+  {
+    "revision": "78aa0676509153305bde072ed1e9cb1c",
+    "url": "/bridge-dealer/static/js/2.88bb370f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f643523fe9804929dc55",
+    "url": "/bridge-dealer/static/js/main.e7da1d6f.chunk.js"
+  },
+  {
+    "revision": "9adc5f1944ea0b793b65",
+    "url": "/bridge-dealer/static/js/runtime-main.e042c523.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/bridge-dealer/static/media/logo.5d5d9eef.svg"
+  }
+]);
