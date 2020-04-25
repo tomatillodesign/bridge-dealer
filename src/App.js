1462032335851b958948e3e06aa0fdb1109ed1fe
tomatillodesign <chris@tomatillodesign.com>
2020-04-25 14:33:49 -0400

    style & markup fixes

--- src/App.js
+++ src/App.js
@@ -6,8 +6,13 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header"><a href="/">Bridge Dealer</a></header>
-      <Landing />
+      <header className="App-header"><h1><a href="/bridge-dealer">Bridge Dealer</a></h1></header>
+      <div className="clb-grail-body">
+          <Landing />
+      </div>
+      <footer className="clb-bridge-dealer-footer">
+              Bridge Dealer &middot; <a href="https://github.com/tomatillodesign/bridge-dealer" target="_blank">Version 1.0</a> &middot; By Chris Liu-Beers, <a href="http://tomatillodesign.com" target="_blank">Tomatillo Design</a>
+            </footer>
     </div>
   );
 }
